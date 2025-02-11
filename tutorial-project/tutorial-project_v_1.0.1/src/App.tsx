01757bdaa96b3d45337aadc2273388386111aedf
SurajNaidu0 <surajangurajnaidu@gmail.com>
2025-02-11 16:00:16 +0530

    adding tutorial for v1.0.1

---
+++ tutorial-project/tutorial-project_v_1.0.1/src/App.tsx
@@ -0,0 +1,31 @@
+import { FormConfig, ConfigComponent } from "@surajnaidu/modern-form-components";
+
+const config: FormConfig[] = [
+    { type: "text", heading: "Enter Your Name" },
+    { type: "password", heading: "Enter Your Password" },
+    { type: "email", heading: "Enter Your Email" },
+    {
+        type: "options",
+        heading: "Select your options",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        minSelection: 1,
+        maxSelection: 3,
+    },
+    {
+        type: "dropdown",
+        heading: "Please select an option",
+        options: ["Option A", "Option B", "Option C", "Option D"],
+    },
+    { type: "date", heading: "Select a Date" },
+];
+
+function App() {
+    return (
+        <div>
+            <ConfigComponent config={config} />
+        </div>
+    );
+}
+
+
+export default App;
